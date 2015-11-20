b22c73e9bfed155569b48502b465436638cf6be8
yhm <yhmcslg@126.com>
2015-11-20 17:17:36 +0800

    rss and paginator

--- article/views.py
+++ article/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
 from django.http import Http404
+from django.contrib.syndication.views import Feed 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def home(request):
@@ -20,7 +22,16 @@ def test(request):
     return render(request,'test.html',{'current_time':datetime.now()})
 
 def home(request):
-    post_list = Article.objects.all()
+    posts = Article.objects.all()
+    paginator = Paginator(posts,2)
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.paginator(paginator.num_pages)
+        
     return render(request,'home.html',{'post_list':post_list})
 
 
@@ -43,3 +54,37 @@ def search_tag(request,tag):
     except Article.DoesNotExist:
         raise Http404
     return render(request,'tag.html',{'post_list':post_list})
+
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+    if not s:
+        return render(request,'home.html')
+    else:
+        post_list = Article.objects.filter(title__icontains = s)
+        if len(post_list) == 0 :
+            return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : True})
+        else:
+            return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : False})
+    return redirect('/')
+
+
+class RSSFeed(Feed) :
+    title = "RSS feed - article"
+    link = "feeds/posts/"
+    description = "RSS feed - blog posts"
+
+    def items(self):
+        return Article.objects.order_by('-date_time')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_pubdate(self, item):
+        return item.date_time
+
+    def item_description(self, item):
+        return item.content
